74ca9503799573fb98ba7d2924682b414d6d8e46
Matt Thompson <mthompson@wspackaging.com>
2012-11-08 18:49:30 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinery::Application.config.secret_token = 'ba0fe6bb105345243dc6dd8e1c3b3387b65f00745b50b8558a168da05892a92a6265fa1aab8137d0b245415bba1770e9076c11a92d9f886a965ab08b590e8933'
